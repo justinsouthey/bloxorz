f719cdf638855c7ec11735e20869289981324bb8
justso <just.so@presentable.me>
2023-05-25 01:46:32 +0200

    Replaces controller with kinematic version

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,40 +1,125 @@
+using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UIElements.Experimental;
+
+public enum MainAxis
+{
+    X,
+    Y,
+    Z
+}
+
+public enum Direction
+{
+    None,
+    Up,
+    Down,
+    Left,
+    Right
+}
 
 public class Block : MonoBehaviour
 {
     [SerializeField] private Rigidbody _rigidbody;
-    [SerializeField] private float _forceFactor = 5;
-    [SerializeField] private float _torqueForceFactor = 5;
+    [SerializeField] private float _rotationTime = .2f;
+
+    private bool _moving = false;
+    private MainAxis _mainAxis = MainAxis.Y;
+    private Transform _pivotTransform;
+    [SerializeField] private Transform _cubeVisual;
+    private Direction _storedDirection;
+
+    private void Awake()
+    {
+        _pivotTransform = new GameObject("PivotHelper").transform;
+    }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            _rigidbody.AddForce(Vector3.forward * _forceFactor);
-            _rigidbody.AddTorque(Vector3.right * _torqueForceFactor, ForceMode.Impulse);
-        }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            _rigidbody.AddForce(Vector3.back * _forceFactor);
-            _rigidbody.AddTorque(Vector3.left * _torqueForceFactor, ForceMode.Impulse);
-        }
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            _rigidbody.AddForce(Vector3.left * _forceFactor);
-            _rigidbody.AddTorque(Vector3.forward * _torqueForceFactor, ForceMode.Impulse);
-        }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            _rigidbody.AddForce(Vector3.right * _forceFactor);
-            _rigidbody.AddTorque(Vector3.back * _torqueForceFactor, ForceMode.Impulse);
-        }
+        if (Input.GetKeyDown(KeyCode.UpArrow)) Rotate(Direction.Up);
+        if (Input.GetKeyDown(KeyCode.DownArrow)) Rotate(Direction.Down);
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) Rotate(Direction.Left);
+        if (Input.GetKeyDown(KeyCode.RightArrow)) Rotate(Direction.Right);
 
+        if (_moving)
+            return;
         if (Input.GetKeyDown(KeyCode.R))
         {
+            _mainAxis = MainAxis.Y;
             _rigidbody.velocity = Vector3.zero;
             _rigidbody.angularVelocity = Vector3.zero;
             transform.position = Vector3.zero;
             transform.rotation = Quaternion.identity;
         }
+
+    }
+
+    private void Rotate(Direction direction)
+    {
+        if (_moving)
+        {
+            _storedDirection = direction;
+            return;
+        }
+
+        _storedDirection = Direction.None;
+        Vector3 pivotPoint;
+        switch (direction)
+        {
+            case Direction.None:
+                break;
+            case Direction.Up:
+                pivotPoint = _cubeVisual.position + Vector3.forward * (_mainAxis == MainAxis.Z ? 1 : .5f);
+                pivotPoint.y = 0;
+                StartCoroutine(RotateCoroutine(pivotPoint, Vector3.right));
+                _mainAxis = _mainAxis == MainAxis.Y ? MainAxis.Z : _mainAxis == MainAxis.Z ? MainAxis.Y : _mainAxis;
+                break;
+            case Direction.Down:
+                pivotPoint = _cubeVisual.position + Vector3.back * (_mainAxis == MainAxis.Z ? 1 : .5f);
+                pivotPoint.y = 0;
+                StartCoroutine(RotateCoroutine(pivotPoint, Vector3.left));
+                _mainAxis = _mainAxis == MainAxis.Y ? MainAxis.Z : _mainAxis == MainAxis.Z ? MainAxis.Y : _mainAxis;
+                break;
+            case Direction.Left:
+                pivotPoint = _cubeVisual.position + Vector3.left * (_mainAxis == MainAxis.X ? 1 : .5f);
+                pivotPoint.y = 0;
+                StartCoroutine(RotateCoroutine(pivotPoint, Vector3.forward));
+                _mainAxis = _mainAxis == MainAxis.Y ? MainAxis.X : _mainAxis == MainAxis.X ? MainAxis.Y : _mainAxis;
+                break;
+            case Direction.Right:
+                pivotPoint = _cubeVisual.position + Vector3.right * (_mainAxis == MainAxis.X ? 1 : .5f);
+                pivotPoint.y = 0;
+                StartCoroutine(RotateCoroutine(pivotPoint, Vector3.back));
+                _mainAxis = _mainAxis == MainAxis.Y ? MainAxis.X : _mainAxis == MainAxis.X ? MainAxis.Y : _mainAxis;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
+        }
+    }
+
+    private IEnumerator RotateCoroutine(Vector3 pivotPoint, Vector3 rotationAxis)
+    {
+        _moving = true;
+        _rigidbody.isKinematic = true;
+        var t = 0f;
+        _pivotTransform.position = pivotPoint;
+        _pivotTransform.rotation = Quaternion.identity;
+        transform.parent = _pivotTransform;
+        while (t < 1)
+        {
+            t += Time.deltaTime / _rotationTime;
+            _pivotTransform.localEulerAngles = rotationAxis * 90 * Easing.InSine(t);
+            yield return null;
+        }
+        _pivotTransform.localEulerAngles = rotationAxis * 90;
+        transform.parent = null;
+        var pos = transform.position;
+        pos.x = Mathf.RoundToInt(pos.x * 2) * .5f;
+        pos.z = Mathf.RoundToInt(pos.z * 2) * .5f;
+        _rigidbody.isKinematic = false;
+        _moving = false;
+        if(_storedDirection != Direction.None)
+            Rotate(_storedDirection);
     }
 }
--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -8,10 +8,34 @@ public class Board : MonoBehaviour
 
     private void Start()
     {
-        for (var z = -3; z <= 3; z++)
-        for (var x = -3; x <= 3; x++)
+        SetupBoard();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
         {
-            var tile = Instantiate(_tilePrefab, new Vector3(x, 0, z), Quaternion.identity, transform);
+            DestroyBoard();
+            SetupBoard();
+        }
+    }
+
+    private void DestroyBoard()
+    {
+        for (var i = 0; i < transform.childCount; i++)
+            Destroy(transform.GetChild(i).gameObject);
+    }
+
+    private void SetupBoard()
+    {
+        for (var z = -6; z <= 6; z++)
+        for (var x = -6; x <= 6; x++)
+        {
+            var pos = new Vector3(x, 0, z);
+            var p = pos.sqrMagnitude / 36;
+            if (Random.value < p)
+                continue;
+            var tile = Instantiate(_tilePrefab, pos, Quaternion.identity, transform);
             if (Mathf.Abs(x + z) % 2 == 0)
                 tile.GetComponentInChildren<MeshRenderer>().material = _dark;
             else
